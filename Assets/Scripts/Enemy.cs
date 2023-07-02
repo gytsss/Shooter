5f77952c409b986247fdc6489b267dac5f8f7295
gytsss <totogodoy@hotmail.com>
2023-07-01 22:40:34 -0300

    fix enemy´s hardcoded values

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,10 +7,20 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    private enum EnemyState
+    {
+        Patrol,
+        Chase,
+        Attack
+    }
+
+    private EnemyState currentState;
+
     [SerializeField] private Slider healthBar;
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] private NavMeshAgent enemy;
     [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private Transform respawnPoint;
     private float currentHealth;
 
     private Transform player;
@@ -38,6 +48,11 @@ public class Enemy : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    private void Start()
+    {
+        currentState = EnemyState.Patrol;
+    }
+
     private void Update()
     {
         distanceToPlayer = Vector3.Distance(transform.position, player.position);
@@ -52,20 +67,65 @@ public class Enemy : MonoBehaviour
         else
             playerInSightRange = true;
 
-        //TODO: TP2 - FSM
-        if (!playerInSightRange && !playerInAttackRange)
-            Patroling();
 
-        if (playerInSightRange && !playerInAttackRange)
-            ChasePlayer();
-
-        if (playerInSightRange && playerInAttackRange)
-            AttackPlayer();
+        switch (currentState)
+        {
+            case EnemyState.Patrol:
+                if (playerInSightRange && !playerInAttackRange)
+                {
+                    currentState = EnemyState.Chase;
+                    ChasePlayer();
+                }
+                else if (playerInSightRange && playerInAttackRange)
+                {
+                    currentState = EnemyState.Attack;
+                    AttackPlayer();
+                }
+                else
+                {
+                    Patroling();
+                }
+                break;
+            case EnemyState.Chase:
+                if (!playerInSightRange)
+                {
+                    currentState = EnemyState.Patrol;
+                    Patroling();
+                }
+                else if (playerInSightRange && playerInAttackRange)
+                {
+                    currentState = EnemyState.Attack;
+                    AttackPlayer();
+                }
+                else
+                {
+                    ChasePlayer();
+                }
+                break;
+            case EnemyState.Attack:
+                if (!playerInAttackRange)
+                {
+                    if (playerInSightRange)
+                    {
+                        currentState = EnemyState.Chase;
+                        ChasePlayer();
+                    }
+                    else
+                    {
+                        currentState = EnemyState.Patrol;
+                        Patroling();
+                    }
+                }
+                else
+                {
+                    AttackPlayer();
+                }
+                break;
+        }
     }
 
     private void Patroling()
     {
-        //TODO: Fix - It's not recommendable to edit class level variables all around your code (which is called "side effects")
         if (enemy.remainingDistance < distanceMargin)
         {
             walkPoint = GetNewRandomPointInsideBounds();
@@ -101,44 +161,6 @@ public class Enemy : MonoBehaviour
 
         return new Vector3(mapCenter.x + randomX, mapCenter.y + randomY, mapCenter.z + randomZ);
 
-    }
-
-    private void SearchWalkPoint()
-    {
-        Vector3 mapCenter = Vector3.zero;
-        Vector3 mapSize = Vector3.one;
-
-
-        //TODO: Fix - Cache value/s
-        mapObject = GameObject.Find("Room");
-
-        mapObject = GameObject.Find("Floor Tile");
-
-        if (mapObject != null)
-        {
-            Renderer mapRenderer = mapObject.GetComponent<Renderer>();
-            if (mapRenderer != null)
-            {
-                mapCenter = mapRenderer.bounds.center;
-                mapSize = mapRenderer.bounds.size;
-            }
-        }
-
-        float randomX = Random.Range(-mapSize.x / 2.1f, mapSize.x / 2.1f);
-        float randomZ = Random.Range(-mapSize.z / 2.1f, mapSize.z / 2.1f);
-        float randomY = Random.Range(-mapSize.y / 2f, mapSize.y / 2f);
-        walkPoint = new Vector3(mapCenter.x + randomX, mapCenter.y + randomY, mapCenter.z + randomZ);
-
-        //TODO: TP2 - Remove unused methods/variables/classes
-        RaycastHit hitInfo;
-
-        //TODO: Fix - Unclear logic - If you're trying to figure if the enemy is able to walk to that point, navmesh has functionality meant for that.
-        if (Physics.Raycast(walkPoint, Vector3.down, out hitInfo, 2f) || Physics.Raycast(walkPoint, Vector3.up, out hitInfo, 2f))
-        {
-            SearchWalkPoint();
-            return;
-        }
-        
     }
 
     private void ChasePlayer()
@@ -179,8 +201,7 @@ public class Enemy : MonoBehaviour
         if (currentHealth <= 0)
         {
             currentHealth = maxHealth;
-            //TODO: Fix - Hardcoded value
-            transform.position = GameObject.Find("EnemyRespawnPoint").transform.position;
+            transform.position = respawnPoint.position;
         }
         UpdateHealthBar();
     }
