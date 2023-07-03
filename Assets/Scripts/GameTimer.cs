9da7d6bb9412178ab0d0834cc64dddbca160c88a
tobi <totogodoy@hotmail.com>
2023-07-03 11:15:31 -0300

    add timer and loss panel

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using UnityEngine;
+using TMPro;
+
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] GameObject lossPanel;
+    [SerializeField] private TextMeshProUGUI remainingTimeText;
+    [SerializeField] private float timeToWin = 15f;
+     private float remainingTime;
+
+    private void Start()
+    {
+        remainingTime = timeToWin;
+        lossPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        remainingTime -= Time.deltaTime;
+
+        remainingTimeText.text = "Time left: " + remainingTime.ToString();
+
+        if (remainingTime <= 0)
+        {
+            ShowLossPanel();
+        }
+
+    }
+
+    private void ShowLossPanel()
+    {
+        Time.timeScale = 0f;
+        lossPanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
